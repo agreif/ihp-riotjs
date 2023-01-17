c8995ef8a7dc79fbdb624bd6cea1783bb631fee3
Alex Greif <alex.greif@gmail.com>
2023-01-17 23:18:22 +0100

    added sample riot tag

---
+++ static/riot/my-tag.js
@@ -0,0 +1,8 @@
+var myTag = {
+  css: null,
+  exports: null,
+  template: (template, expressionTypes, bindingTypes, getComponent) => template('\n  Hello\n', []),
+  name: 'my-tag'
+};
+
+export { myTag as default };
